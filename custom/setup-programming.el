2ef313d14833def1cf3579ff3e62cdce09c5328b
zhuchong530 <zhuchong530@gmail.com>
2017-08-28 22:06:46 +0800

    add some golang config, even I don't know the meaning

--- custom/setup-programming.el
+++ custom/setup-programming.el
@@ -1,3 +1,23 @@
+(use-package cc-mode
+  :config
+;; Available C style:
+;; “gnu”: The default style for GNU projects
+;; “k&r”: What Kernighan and Ritchie, the authors of C used in their book
+;; “bsd”: What BSD developers use, aka “Allman style” after Eric Allman.
+;; “whitesmith”: Popularized by the examples that came with Whitesmiths C, an early commercial C compiler.
+;; “stroustrup”: What Stroustrup, the author of C++ used in his book
+;; “ellemtel”: Popular C++ coding standards as defined by “Programming in C++, Rules and Recommendations,” Erik Nyquist and Mats Henricson, Ellemtel
+;; “linux”: What the Linux developers use for kernel development
+;; “python”: What Python developers use for extension modules
+;; “java”: The default style for java-mode (see below)
+;; “user”: When you want to define your own style
+  (setq c-default-style "k&r") ;; set style to "k&r"
+  (setq c-basic-offset 4)
+  (global-set-key (kbd "RET") 'newline-and-indent)  ; automatically indent when press RET
+  (setq gdb-many-windows t ;; use gdb-many-windows by default
+        gdb-show-main t))  ;; Non-nil means display source file containing the main routine at startup
+
+
 ;; function-args
 (use-package function-args
   :disabled
@@ -31,17 +51,29 @@
   )
 
 ;; Package - company-irony
-(eval-after-load 'company
-  '(add-to-list 'company-backends 'company-irony))
+(use-package company-irony
+  :after company-mode
+  :config
+  (add-to-list 'company-backends 'company-irony))
+
+;; Package - company-irony-c-headers
+(use-package company-irony-c-headers
+  :ensure t
+  :after company-mode
+  :config
+  (add-to-list 'company-backends '(company-irony-c-headers company-irony))
+  )
 
 ;; Package - flycheck-irony
-(eval-after-load 'flycheck
-  '(add-hook 'flycheck-mode-hook #'flycheck-irony-setup))
-(add-hook 'c++-mode-hook 'flycheck-mode)
-(add-hook 'c-mode-hook 'flycheck-mode)
-(add-hook 'c++-mode-hook
-          (lambda () (setq flycheck-clang-include-path
-                           (list (expand-file-name "/usr/include")))))
+(use-package flycheck-irony
+  :after flycheck-mode
+  :config
+  (add-hook 'flycheck-mode-hook  #'flycheck-irony-setup)
+  (add-hook 'c++-mode-hook 'flycheck-mode)
+  (add-hook 'c-mode-hook 'flycheck-mode)
+  (add-hook 'c++-mode-hook
+            (lambda () (setq flycheck-clang-include-path
+                             (list (expand-file-name "/usr/include"))))))
 
 ;; Package - irony-eldoc
 (use-package irony-eldoc
@@ -49,28 +81,11 @@
   :config (add-hook 'irony-mode-hook 'irony-eldoc)
   )
 
-;; Package - company-irony-c-headers
-(use-package company-irony-c-headers
-  :ensure t
-  )
-
 ;; hs-minor-mode for folding source code
 (add-hook 'c-mode-common-hook 'hs-minor-mode)
+(add-hook 'c++-mode-hook 'irony-mode)
+(add-hook 'c-mode-hook 'irony-mode)
 
-;; Available C style:
-;; “gnu”: The default style for GNU projects
-;; “k&r”: What Kernighan and Ritchie, the authors of C used in their book
-;; “bsd”: What BSD developers use, aka “Allman style” after Eric Allman.
-;; “whitesmith”: Popularized by the examples that came with Whitesmiths C, an early commercial C compiler.
-;; “stroustrup”: What Stroustrup, the author of C++ used in his book
-;; “ellemtel”: Popular C++ coding standards as defined by “Programming in C++, Rules and Recommendations,” Erik Nyquist and Mats Henricson, Ellemtel
-;; “linux”: What the Linux developers use for kernel development
-;; “python”: What Python developers use for extension modules
-;; “java”: The default style for java-mode (see below)
-;; “user”: When you want to define your own style
-(setq c-default-style "k&r)" ;; set style to "linux"
-(setq c-basic-offset 4)
-(global-set-key (kbd "RET") 'newline-and-indent)  ; automatically indent when press RET
 
 ;; activate whitespace-mode to view all whitespace characters
 (global-set-key (kbd "C-c w") 'whitespace-mode)
@@ -90,13 +105,7 @@
                                (setq-local compilation-read-command nil)
                                (call-interactively 'compile)))
 
-;; setup GDB
-(setq
- ;; use gdb-many-windows by default
- gdb-many-windows t
- ;; Non-nil means display source file containing the main routine at startup
- gdb-show-main t
- )
+
 
 ;; Package: projejctile
 (use-package projejctile
@@ -121,13 +130,10 @@
 ;; Package zygospore
 (global-set-key (kbd "C-x 1") 'zygospore-toggle-delete-other-windows)
 
-
+;; uniquify
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'post-forward uniquify-separator ":")
 
-(add-hook 'c++-mode-hook 'irony-mode)
-(add-hook 'c-mode-hook 'irony-mode)
-;; (add-hook 'objc-mode-hook 'irony-mode)
 ;; replace the `completion-at-point' and `completion-symbol' bindings in
 ;; irony-mode's buffers by irony-modes function
 (defun my-irony-mode-hook()
@@ -144,43 +150,100 @@
 
 
 ;;go-mode packages
+;; REQUIREMENTS:
+;; go get -u golang.org/x/tools/cmd/...
+;; go get -u github.com/rogpeppe/godef
+;; go get -u github.com/nsf/gocode
+;; go get -u github.com/kisielk/errcheck
+
 (use-package go-mode
   :ensure t
+  :config
+  ;; Use goimports instead of go-fmt
+  (setq gofmt-command "goimports")
+  (add-hook 'go-mode-hook 'company-mode)
+  ;; Call gofmt before saving
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (add-hook 'go-mode-hook 'setup-go-mode-compile)
+  (add-hook 'go-mode-hook #'smartparens-mode)
+  (add-hook 'go-mode-hook (lambda ()
+                            (setq tab-width 4)
+                            (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+                            (local-set-key (kbd "C-c C-g") 'go-goto-imports)
+                            (local-set-key (kbd "C-c C-k") 'godoc)
+
+                            (set (make-local-variable 'company-backends) '(company-go))
+                            (company-go)))
   :mode ("\\.go\\'" . go-mode)
   )
-;removes all unused imports
-(add-hook 'go-mode-hook '(lambda() (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
-;; format the current buffer
-(add-hook 'go-mode-hook '(lambda() (local-set-key (kbd "C-c C-f") 'gofmt)))
-;; format the buffer when save
-(add-hook 'before-save-hook 'gofmt-before-save)
-;; show the go documentation for a given package
-(add-hook 'go-mode-hook '(lambda() (local-set-key (kbd "C-c C-k") 'godoc)))
 
-;; gocode autocomplete
-(add-hook 'go-mode-hook '(lambda()
-                           (set (make-local-variable 'company-backends) '(company-go))
-                           (company-mode)))
+(use-package go-errcheck)
+
+(use-package go-add-tags)
+
+(use-package company-go
+  :after go-mode
+  :bind (:map go-mode-map
+              ;Godef jump key binding
+              ("M-." . godef-jump)))
+
+(use-package flycheck-go)
+
 ;; go-eldoc packages
 (use-package go-eldoc
-  :ensure t)
-(add-hook 'go-mode-hook 'go-eldoc-setup)
-(set-face-attribute 'eldoc-highlight-function-argument nil
-                    :underline t :foreground "green"
-                    :weight 'bold)
+  :ensure t
+  :config
+  (add-hook 'go-mode-hook 'go-eldoc-setup)
+  (set-face-attribute 'eldoc-highlight-function-argument nil
+                      :underline t :foreground "green"
+                      :weight 'bold)
+  )
+
+(use-package flycheck-gometalinter
+  :ensure t
+  :config
+  (progn
+    (flycheck-gometalinter-setup))
+  ;; skips 'vendor' directories and sets GO15VENDOREXPERIMENT=1
+  (setq flycheck-gometalinter-vendor t)
+  ;; only show errors
+  (setq flycheck-gometalinter-errors-only t)
+  ;; only run fast linters
+  (setq flycheck-gometalinter-fast t)
+  ;; use in tests files
+  (setq flycheck-gometalinter-test t)
+  ;; Only enable selected linters
+  (setq flycheck-gometalinter-enable-linters '("golint" "gotype" "gocyclo"))
+  ;; Set different deadline (default: 5s)
+  (setq flycheck-gometalinter-deadline "10s"))
+
 ;; go-guru packages
 (use-package go-guru
-  :ensure t)
-(go-guru-hl-identifier-mode)
-(add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
+  :ensure t
+  :config
+  (go-guru-hl-identifier-mode)
+  (add-hook 'go-mode-hook #'go-guru-hl-identifier-mode)
+  )
+
+(defun setup-go-mode-compile()
+  ;; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet")))
+
 
 ;;magit package
 (use-package magit
   :ensure t
   :commands magit-get-top-dir
-  :bind (("C-c g" . magit-status)
-         ("C-c C-g l" . magit-log-buffer-file)
-         ("C-c f" . magit-grep))
+  :bind (("C-x g s" . magit-status)
+         ("C-x g f" . magit-log-buffer-file)
+         ("C-x g x" . magit-checkout)
+         ("C-x g c" . magit-commit)
+         ("C-x g p" . magit-push)
+         ("C-x g u" . magit-pull)
+         ("C-x g e" . magit-ediff-resolve)
+         ("C-x g r" . magit-rebase-interactive))
   :config (magit-auto-revert-mode)
   )
 

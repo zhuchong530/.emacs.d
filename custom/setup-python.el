057c7fab4326bc2d14be96b1c9322c52568c6669
zhuchong530 <zhuchong530@gmail.com>
2020-10-14 11:20:33 +0800

    set python-interpreter to python3, otherwise it use python2 in python-mode

--- custom/setup-python.el
+++ custom/setup-python.el
@@ -5,9 +5,11 @@
 (use-package python
   :defer t
   :mode ("\\.py\\'" . python-mode)
-  :interpreter ("python3" . python-mode)
+  ;; :interpreter ("python3" . python-mode)
   :init
   (add-hook 'python-mode-hook 'hs-minor-mode)
+  :config
+  (setq python-shell-interpreter "python3")
   )
 ;; python3.3 build-in virtualenv environments
 (use-package pyvenv
@@ -47,9 +49,9 @@
   ;; (flycheck-python-flake8-executable "/usr/bin/flake8-3.4")
   (add-hook 'python-mode-hook 'elpy-mode)
   ;; (setq python-check-command "flake8")
-  ;; (setq elpy-rpc-backend "jedi")
+  (setq elpy-rpc-backend "jedi")
   ;; (setq elpy-use-cpython "/usr/bin/python3")
-  ;; (setq elpy-rpc-python-command "/usr/bin/python3")
+  (setq elpy-rpc-python-command "python3")
   ;; (setq python-shell-interpreter "/usr/bin/python3")
   )
 

fc45da4f922dfba9ce116f5c6ec972d06c3de159
zhuchong530 <zhuchong530@gmail.com>
2023-03-05 09:52:26 +0800

    add eaf config, update consult config, and some small update.

--- init.el
+++ init.el
@@ -10,13 +10,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(uniquify zeal-at-point youdao-dictionary yasnippet ws-butler which-key web-mode volatile-highlights vertico use-package undo-tree toc-org rustic realgud-lldb rainbow-mode rainbow-delimiters pyvenv projectile posframe ox-reveal ox-gfm org-roam-ui org-mime org-download org-bullets orderless ob-go nyan-mode nasm-mode marginalia magit lsp-mode js2-mode iedit highlight-symbol highlight-indent-guides helpful golden-ratio go-eldoc git-gutter+ gcmh flycheck-rust exec-path-from-shell doom-themes doom-modeline diminish corfu consult comment-dwim-2 cmake-mode clean-aindent-mode centaur-tabs cargo cape all-the-icons-dired all-the-icons-completion ace-window ace-jump-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :foreground "red" :height 5.0)))))
+   '(uniquify zeal-at-point youdao-dictionary yasnippet ws-butler which-key web-mode volatile-highlights vertico use-package undo-tree toc-org rustic realgud-lldb rainbow-mode rainbow-delimiters pyvenv projectile posframe ox-reveal ox-gfm org-roam-ui org-mime org-download org-bullets orderless ob-go nyan-mode nasm-mode marginalia magit lsp-mode js2-mode iedit highlight-symbol highlight-indent-guides helpful golden-ratio go-eldoc git-gutter+ gcmh flycheck-rust exec-path-from-shell doom-themes doom-modeline diminish corfu consult comment-dwim-2 cmake-mode clean-aindent-mode centaur-tabs cargo cape all-the-icons-dired all-the-icons-completion ace-window)))
 
 (provide 'init)
 ;;; init.el ends here

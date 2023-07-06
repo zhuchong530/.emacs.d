9e9ba32d5c905e46c7b20eea4299989d1e9c27ac
zhuchong530 <zhuchong530@gmail.com>
2023-07-06 19:05:10 +0800

    add transwin package, for frame transparency

--- init.el
+++ init.el
@@ -10,7 +10,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-roam vertico-posframe olivetti treemacs uniquify zeal-at-point youdao-dictionary yasnippet ws-butler which-key web-mode volatile-highlights vertico use-package undo-tree toc-org rustic realgud-lldb rainbow-mode rainbow-delimiters pyvenv projectile posframe ox-reveal ox-gfm org-mime org-download org-bullets orderless ob-go nyan-mode nasm-mode marginalia magit lsp-mode js2-mode iedit highlight-symbol highlight-indent-guides helpful golden-ratio go-eldoc git-gutter+ gcmh flycheck-rust exec-path-from-shell doom-themes doom-modeline diminish corfu consult comment-dwim-2 cmake-mode clean-aindent-mode centaur-tabs cargo cape all-the-icons-dired all-the-icons-completion ace-window)))
+   '(transwin org-roam vertico-posframe olivetti treemacs uniquify zeal-at-point youdao-dictionary yasnippet ws-butler which-key web-mode volatile-highlights vertico use-package undo-tree toc-org rustic realgud-lldb rainbow-mode rainbow-delimiters pyvenv projectile posframe ox-reveal ox-gfm org-mime org-download org-bullets orderless ob-go nyan-mode nasm-mode marginalia magit lsp-mode js2-mode iedit highlight-symbol highlight-indent-guides helpful golden-ratio go-eldoc git-gutter+ gcmh flycheck-rust exec-path-from-shell doom-themes doom-modeline diminish corfu consult comment-dwim-2 cmake-mode clean-aindent-mode centaur-tabs cargo cape all-the-icons-dired all-the-icons-completion ace-window)))
 
 (provide 'init)
 ;;; init.el ends here

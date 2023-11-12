07fa62c2de1531ea7575ce76d63e836906bbe0f4
Cathy.Chang <zhuchong530@gmail.com>
2023-11-12 21:12:15 +0800

    add gc memory size, open use-package report logger

--- early-init.el
+++ early-init.el
@@ -1,34 +1,41 @@
 ;;; package -- Summary
 ;;; Commentary:
-;;; early-init.el --- Early Init File
-;;; Speed up the Emacs startup time, Some parts are stolen from doom-emacs
+;; early-init.el --- Early Init File
+;; Emacs 27 introduces early-init.el, which is run before init.el,
+;; before package and UI initialization happens.
+;;
+
 ;;; Code:
-;; Make startup faster by reducing the frequency of garbage
-;; collection. The default is 800 kilobytes
+
+
+
+;; Garbage Collection
+;; A big contributer to startup times is garbage collection. We up the gc
+;; threshold to temporarily prevent it from running, then reset it later.
+(setq gc-cons-percentage-original gc-cons-percentage
+      gc-cons-threshold-original gc-cons-threshold)
 (setq gc-cons-threshold most-positive-fixnum
       gc-cons-percentage 0.6)
 
-;; Increase the amount of data which Emacs reads from the process.
-;; the default is 4 kilobytes make it 32 Megabyte
-(setq read-process-output-max (* 1024 1024 32)) ; 32MB
-
-;; In Emacs 27+, package initialization occurs before `user-init-file` is
-;; loaded, but after `early-init-file`. Doom handles package initialization, so
-;; we must prevent Emacs from doing it early
-;; (advice-add #'package--ensure-init-file :override #'ignore)
-(setq package-enable-at-startup nil)
 ;; Do not allow loading from the package cache (same reason).
 (setq package-quickstart nil)
-
-;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
-(push '(menu-bar-lines . 0) default-frame-alist)
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(vertical-scroll-bars) default-frame-alist)
 
 ;; Resizing the Emacs frame can be terribly expensive part of changing the
 ;; font. By inhibiting this, we easily halve startup times with fonts that are
 ;; larger than the system default.
 (setq frame-inhibit-implied-resize t)
+
+;; Increase the amount of data which Emacs reads from the process.
+;; the default is 4 kilobytes make it 32 Megabyte
+(setq read-process-output-max (* 1024 1024 32)) ; 32MB
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+(push '(fullscreen . maximized) default-frame-alist)
+(push '(underorated . t) default-frame-alist)
+
 
 ;; Disable GUI elements
 (menu-bar-mode -1)
@@ -37,6 +44,15 @@
 (setq inhibit-splash-screen t)
 (setq use-file-dialog nil)
 
+;; native-comp settings
+(when (and (featurep 'native-comp-available-p)
+           (native-comp-available-p))
+  (startup-redirect-eln-cache (expand-file-name "eln-cache/" user-emacs-directory))
+  (setq-default native-comp-speed 2
+               native-comp-async-query-on-exit t
+               native-comp-jit-compilation nil
+               native-comp-async-report-warnings-errors nil))
+
 ;; Prevent unwanted runtime builds in gcemacs (native-comp); packages are
 ;; compiled ahead-of-time when they are installed and site files are compiled
 ;; when gccemacs is installed
@@ -44,7 +60,7 @@
 
 
 ;; add the module path
-(add-to-list 'load-path (expand-file-name "custom" user-emacs-directory))
+;; (add-to-list 'load-path (expand-file-name "custom" user-emacs-directory))
 ;; (add-to-list 'load-path "~/.emacs.d/custom")
 ;; (add-to-list 'load-path (concat user-emacs-directory "custom"))
 

4de9e3aa5e15dd9633d6ae2b7bd94e80b1af0a24
zhuchong530 <zhuchong530@gmail.com>
2024-06-29 21:12:12 +0800

    global-set-key for citre functions

--- custom.el
+++ custom.el
@@ -6,8 +6,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7c28419e963b04bf7ad14f3d8f6655c078de75e4944843ef9522dbecfcd8717d" "8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "c8b3d9364302b16318e0f231981e94cbe4806cb5cde5732c3e5c3e05e1472434" "a9eeab09d61fef94084a95f82557e147d9630fbbb82a837f971f83e66e21e5ad" "9d29a302302cce971d988eb51bd17c1d2be6cd68305710446f658958c0640f68" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" default))
  '(package-selected-packages
-   '(citre treesit-auto cape corfu eglot yasnippet which-key web-mode volatile-highlights vertico-posframe undo-tree transwin toc-org rust-mode rainbow-mode rainbow-delimiters pyvenv ox-reveal ox-gfm org-roam org-mime org-download org-bullets orderless ob-go nyan-mode nasm-mode markdown-mode marginalia magit js2-mode iedit highlight-symbol highlight-indent-guides helpful golden-ratio go-eldoc git-gutter+ gcmh flycheck-rust fanyi exec-path-from-shell doom-themes doom-modeline devdocs crux consult comment-dwim-2 cmake-mode centaur-tabs all-the-icons-dired all-the-icons-completion aggressive-indent ace-window)))
+   '(yasnippet which-key web-mode volatile-highlights vertico-posframe undo-tree treesit-auto transwin tomelr toc-org rust-mode request realgud-lldb rainbow-mode rainbow-delimiters pyvenv popup pkg-info ox-reveal ox-gfm org-roam org-mime org-download org-bullets orderless ob-go nyan-mode nasm-mode markdown-mode marginalia magit js2-mode iedit highlight-symbol highlight-indent-guides helpful golden-ratio go-eldoc git-gutter+ gcmh flycheck-rust fanyi exec-path-from-shell eglot doom-themes doom-modeline devdocs crux corfu consult comment-dwim-2 cmake-mode citre centaur-tabs cape all-the-icons-dired all-the-icons-completion aggressive-indent ace-window)))
 
 (provide 'init)
 ;;; init.el ends here

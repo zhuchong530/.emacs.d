0961ad0e886a062542a8f296171029e397c35673
zhuchong530 <zhuchong530@gmail.com>
2025-01-10 10:11:52 +0800

    add zig language support

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(all-the-icons-completion org-download org-roam cape centaur-tabs comment-dwim-2 devdocs fanyi flycheck-rust helpful magit marginalia ace-window all-the-icons-dired doom-modeline consult yasnippet with-editor which-key web-mode volatile-highlights vertico-posframe undo-tree treesit-auto transwin toc-org rust-mode rainbow-mode rainbow-delimiters pyvenv ox-reveal ox-gfm org-mime org-bullets orderless ob-rust ob-go nyan-mode nerd-icons nasm-mode modern-cpp-font-lock markdown-mode js2-mode iedit golden-ratio go-mode gcmh flycheck f exec-path-from-shell doom-themes crux corfu cmake-mode citre avy async all-the-icons aggressive-indent)))
+   '(zig-mode all-the-icons-completion org-download org-roam cape centaur-tabs comment-dwim-2 devdocs fanyi flycheck-rust helpful magit marginalia ace-window all-the-icons-dired doom-modeline consult yasnippet with-editor which-key web-mode volatile-highlights vertico-posframe undo-tree treesit-auto transwin toc-org rust-mode rainbow-mode rainbow-delimiters pyvenv ox-reveal ox-gfm org-mime org-bullets orderless ob-rust ob-go nyan-mode nerd-icons nasm-mode modern-cpp-font-lock markdown-mode js2-mode iedit golden-ratio go-mode gcmh flycheck f exec-path-from-shell doom-themes crux corfu cmake-mode citre avy async all-the-icons aggressive-indent)))
 
 (provide 'init)
 ;;; init.el ends here

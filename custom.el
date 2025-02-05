317becf75520721bd0d2bee62802ffc083211293
zhuchong530 <zhuchong530@gmail.com>
2025-02-05 10:47:38 +0800

    zig-ts-mode doesn't work well, comment it first

--- custom.el
+++ custom.el
@@ -6,6 +6,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("9d5124bef86c2348d7d4774ca384ae7b6027ff7f6eb3c401378e298ce605f83a" "571661a9d205cb32dfed5566019ad54f5bb3415d2d88f7ea1d00c7c794e70a36" "7c28419e963b04bf7ad14f3d8f6655c078de75e4944843ef9522dbecfcd8717d" "dccf4a8f1aaf5f24d2ab63af1aa75fd9d535c83377f8e26380162e888be0c6a9" "4e2e42e9306813763e2e62f115da71b485458a36e8b4c24e17a2168c45c9cf9d" "4d5d11bfef87416d85673947e3ca3d3d5d985ad57b02a7bb2e32beaf785a100e" default))
  '(package-selected-packages
    '(zig-mode all-the-icons-completion org-download org-roam cape centaur-tabs comment-dwim-2 devdocs fanyi flycheck-rust helpful magit marginalia ace-window all-the-icons-dired doom-modeline consult yasnippet with-editor which-key web-mode volatile-highlights vertico-posframe undo-tree treesit-auto transwin toc-org rust-mode rainbow-mode rainbow-delimiters pyvenv ox-reveal ox-gfm org-mime org-bullets orderless ob-rust ob-go nyan-mode nerd-icons nasm-mode modern-cpp-font-lock markdown-mode js2-mode iedit golden-ratio go-mode gcmh flycheck f exec-path-from-shell doom-themes crux corfu cmake-mode citre avy async all-the-icons aggressive-indent)))
 

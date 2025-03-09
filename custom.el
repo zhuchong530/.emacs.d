0c151b151abe2aa4ef759d99c6573e631906f3b1
Cathy Chang <zhuchong530@gmail.com>
2025-03-09 20:03:31 +0800

    remove nyan-mode package

--- custom.el
+++ custom.el
@@ -6,7 +6,19 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(ace-window aggressive-indent all-the-icons-completion
+                all-the-icons-dired cape centaur-tabs citre cmake-mode
+                comment-dwim-2 compiler-exploere compiler-explorer
+                consult corfu crux devdocs doom-modeline doom-themes
+                exec-path-from-shell fanyi flycheck-rust gcmh go-eldoc
+                golden-ratio helpful iedit js2-mode magit marginalia
+                markdown-mode modern-cpp-font-lock nasm-mode nyan-mode
+                ob-go ob-rust orderless org-bullets org-download
+                org-mime org-roam ox-gfm ox-reveal pyvenv
+                rainbow-delimiters rainbow-mode rust-mode toc-org
+                transwin treesit-auto undo-tree vertico-posframe
+                volatile-highlights web-mode yasnippet zig-mode)))
 
 (provide 'init)
 ;;; init.el ends here

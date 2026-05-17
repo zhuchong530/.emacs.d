99f1e1d114e699b1ffabfa59165bd122863f3a81
zhuchong530 <zhuchong530@gmail.com>
2026-05-17 20:02:20 +0800

    remove some lost command

--- custom.el
+++ custom.el
@@ -13,7 +13,7 @@
                 doom-modeline doom-themes eldoc-box
                 exec-path-from-shell fanyi flycheck-eglot
                 flycheck-rust go-eldoc golden-ratio helpful iedit
-                js2-mode magit marginalia markdown-mode
+                js2-mode magit marginalia markdown-mode mathjax
                 modern-cpp-font-lock nasm-mode nyan-mode ob-go ob-rust
                 orderless org-bullets org-download org-mime org-roam
                 ox-gfm ox-reveal pyvenv rainbow-delimiters
@@ -28,7 +28,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :foreground "red" :height 7.0)))))
+ )
 
 (provide 'custom)
 

0d7736ca07c8270a4be77d26579d0d379757eecc
Cathy.Chang <zhuchong530@gmail.com>
2026-06-30 13:30:16 +0800

    add latex support, just test on WSL2+openSUSE-Tumbleweed, need improvement

--- custom.el
+++ custom.el
@@ -7,19 +7,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ace-window aggressive-indent all-the-icons-completion
-                all-the-icons-dired cape centaur-tabs citre cmake-mode
-                comment-dwim-2 consult corfu crux devdocs
-                doom-modeline doom-themes eldoc-box
-                exec-path-from-shell fanyi flycheck-eglot
-                flycheck-rust go-eldoc golden-ratio helpful iedit
-                js2-mode magit marginalia markdown-mode mathjax
-                modern-cpp-font-lock nasm-mode nyan-mode ob-go ob-rust
-                orderless org-bullets org-download org-mime org-roam
-                ox-gfm ox-reveal pyvenv rainbow-delimiters
-                rainbow-mode rust-mode toc-org transwin treesit-auto
-                undo-tree vertico-posframe volatile-highlights
-                web-mode yasnippet zig-mode)))
+   '(0x0 ace-window aggressive-indent all-the-icons-completion
+         all-the-icons-dired auctex auctex-latexmk cape centaur-tabs
+         citre cmake-mode comment-dwim-2 consult corfu crux devdocs
+         doom-modeline doom-themes eldoc-box exec-path-from-shell
+         fanyi flycheck-eglot flycheck-rust go-mode golden-ratio
+         helpful iedit js2-mode magit marginalia markdown-mode
+         modern-cpp-font-lock nasm-mode ob-go ob-rust orderless
+         org-bullets org-download org-mime org-roam ox-gfm ox-reveal
+         pdf-tools pyvenv rainbow-delimiters rainbow-mode rime
+         rust-mode toc-org transwin treesit-auto undo-tree
+         vertico-posframe volatile-highlights web-mode yasnippet
+         zig-mode)))
 
 (provide 'init)
 ;;; init.el ends here
@@ -28,7 +27,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :foreground "red" :height 7.0)))))
 
 (provide 'custom)
 

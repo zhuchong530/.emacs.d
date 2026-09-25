f0b02110a963c235cee70eabb8a3f61e02b6d275
Cathy.Chang <zhuchong530@gmail.com>
2026-09-25 19:04:41 +0800

    fix mis-spell and bug

--- custom.el
+++ custom.el
@@ -7,16 +7,18 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(0x0 ace-window aggressive-indent auctex-latexmk cape centaur-tabs
-         citre cmake-mode comment-dwim-2 consult corfu crux devdocs
-         doom-modeline eldoc-box exec-path-from-shell fanyi
-         flycheck-eglot flycheck-rust go-mode golden-ratio helpful
-         iedit js2-mode marginalia markdown-mode modern-cpp-font-lock
-         nasm-mode ob-go ob-rust orderless org-bullets org-download
-         org-mime org-roam ox-gfm ox-reveal pdf-tools pyvenv
-         rainbow-delimiters rainbow-mode rime rust-mode toc-org
-         transwin treesit-auto undo-tree vertico-posframe
-         volatile-highlights web-mode yasnippet zig-mode)))
+   '(ace-window aggressive-indent auctex-latexmk cape centaur-tabs citre
+                cmake-mode comment-dwim-2 consult corfu crux
+                doom-modeline doom-themes eglot eldoc-box
+                exec-path-from-shell fanyi flycheck-rust go-mode
+                golden-ratio helpful iedit js2-mode magit marginalia
+                markdown-mode modern-cpp-font-lock nasm-mode
+                nerd-icons-completion nerd-icons-dired ob-go ob-rust
+                orderless org-bullets org-download org-mime org-roam
+                ox-gfm ox-reveal pyvenv queue rainbow-delimiters
+                rainbow-mode reformatter rime rust-mode tablist
+                toc-org transwin treesit-auto vertico-posframe
+                web-mode yasnippet)))
 
 (provide 'init)
 ;;; init.el ends here
